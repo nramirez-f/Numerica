0503f88ee9a18385710014710841d50030d3602d
Nramirez <nramirez@uma.es>
2025-05-05 16:29:39 +0200

    fix(requires): change name to last one

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=[
         "ncviewer @ git+https://github.com/nramirez-f/NcViewer.git@main#egg=ncviewer",
         "fdm @ git+https://github.com/nramirez-f/Finite-Difference.git@main#egg=fdm",
-        "ic @ git+https://github.com/nramirez-f/Initial-Conditions.git@main#egg=ic",
+        "initial_conditions @ git+https://github.com/nramirez-f/Initial_Conditions.git@main#egg=initial_conditions",
     ],
     author="Nramirez",
     description="Numerical tools repository",
